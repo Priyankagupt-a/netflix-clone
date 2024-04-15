1ecf6f6b5014e870f566c89ef38564ebeb0abe60
Priyanka <Priyanka24017@gmail.com>
2024-04-15 16:26:42 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-        
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        /header> < /
+        div >
+    );
 }
 
 export default App;
